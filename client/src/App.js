b81027b6a63348b01fe06f829ca789631342392a
Elvis Moses Tangasaiti <elvis.moses@student.moringaschool.com>
2024-08-07 10:12:17 +0300

    defines routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import WelcomePage from './components /welcomePage';
 import './App.css';
 import MealPage from './components /Mealpage';
 import Menu from './components /Menu';
+import RegisterPage from './components /authenticaton/registerPage';
+import LoginPage from './components /authenticaton/loginpage';
+
 
 function App() {
   return (
@@ -12,6 +15,8 @@ function App() {
         <Route path="/welcome" element={<WelcomePage />} />
         <Route path="categories/:id/meals" element={<Menu />} />
         <Route path="meals/:id" element={<MealPage />} />
+        <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
         <Route path="*" element={<Navigate to="/welcome" />} />
       </Routes>
     </Router>
